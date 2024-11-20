ca4b8bf5aa27a2103e30daa28358addc556e01ea
Ruvikv <mattdaemontmf@gmail.com>
2024-11-20 13:12:26 -0300

    login

---
+++ src/components/login/loginRegister.jsx
@@ -0,0 +1,100 @@
+import React, { useRef, useEffect } from 'react';
+import './style.css';
+import GoogleIcon from '@mui/icons-material/Google';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+const LoginRegister = () => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    const handleSignUp = () => {
+      container.classList.add('right-panel-active');
+    };
+
+    const handleSignIn = () => {
+      container.classList.remove('right-panel-active');
+    };
+
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+
+    signUpButton.addEventListener('click', handleSignUp);
+    signInButton.addEventListener('click', handleSignIn);
+
+    // Cleanup
+    return () => {
+      signUpButton.removeEventListener('click', handleSignUp);
+      signInButton.removeEventListener('click', handleSignIn);
+    };
+  }, []);
+
+  return (
+    <div className="container" id="container" ref={containerRef}>
+      <div className="form-container sign-up-container">
+        <form action="#">
+          <h1>Crea tu Cuenta</h1>
+          <div className="social-container">
+            <a href="#" className="social">
+              <i className="fab fa-facebook-f"><FacebookRoundedIcon fontSize='large'/></i>
+            </a>
+            <a href="#" className="social">
+              <i className="fab fa-google" id="red"><GoogleIcon fontSize='large'/></i>
+            </a>
+            <a href="#" className="social">
+              <i className="fab fa-linkedin-in"><LinkedInIcon  fontSize='large'/></i>
+            </a>
+          </div>
+          <span>o usa tu email como registro</span>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button id="lila">Registrar</button>
+        </form>
+      </div>
+      <div className="form-container sign-in-container">
+        <form action="#">
+          <h1>Iniciar Sesión</h1>
+          <div className="social-container">
+            <a href="#" className="social">
+              <i className="fab fa-facebook-f"><FacebookRoundedIcon fontSize='large'/></i>
+            </a>
+            <a href="#" className="social">
+              <i className="fab fa-google" id="red"><GoogleIcon fontSize='large'/></i>
+            </a>
+            <a href="#" className="social">
+              <i className="fab fa-linkedin-in"><LinkedInIcon fontSize='large' /></i>
+            </a>
+          </div>
+          <span>o usa tu email</span>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Olvidaste tu contraseña?</a>
+          <button>Iniciar sesión</button>
+        </form>
+      </div>
+      <div className="overlay-container">
+        <div className="overlay">
+          <div className="overlay-panel overlay-left">
+            <h1>¡Bienvenido!</h1>
+            <p>Inicia sesión con tu cuenta</p>
+            <button className="ghost" id="signIn">
+              Inicia sesión
+            </button>
+          </div>
+          <div className="overlay-panel overlay-right">
+            <h1>Hola!</h1>
+            <p>Crear una cuenta</p>
+            <button className="ghost" id="signUp">
+              Registrar
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginRegister;
